fa82ffed541175cdca7cd02d610b11b336ed679d
Brad Edmonds <bcedmonds0417@gmail.com>
2021-02-23 19:50:39 -0600

    install tty gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/show_products" => "products#show_all_products"
     get "/show_one_product" => "products#show_product"
-  
   end
 end
