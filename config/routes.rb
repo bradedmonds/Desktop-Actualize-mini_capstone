e4184dcd489ce8d4df740d02998e8a82d580ea2a
Brad Edmonds <bcedmonds0417@gmail.com>
2021-02-23 19:05:20 -0600

    make products controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/show_products" => "show_all_products#index"
+    get "/show_products" => "show_products#index"
+  
+  end
 end
