42008f68953b15227b0ea730c490717fd2832c01
Brad Edmonds <bcedmonds0417@gmail.com>
2021-03-07 10:32:37 -0600

    make suppliers table, migrate table

--- db/migrate/20210303020946_add_inventory_to_products.rb
+++ db/migrate/20210303020946_add_inventory_to_products.rb
@@ -1,5 +1,6 @@
 class AddInventoryToProducts < ActiveRecord::Migration[6.1]
   def change
     add_column :products, :inventory, :integer
+    add_column :products, :supplier_id, :integer
   end
 end
